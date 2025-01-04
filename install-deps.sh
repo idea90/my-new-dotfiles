ee7a719c4c4b4b6e5781415fa3672d53f8292290
idea90 <rtgopkbvn@gmail.com>
2025-01-05 01:58:10 +0700

    .

---
+++ install-deps.sh
@@ -0,0 +1,2 @@
+yay -Syu
+yay -S hyprland sddm waybar python-pywal16 swww alacritty kitty waybar-module-pacman-updates-git xed neovim rofi-wayland wlogout hyprlock swaync zen-browser-bin thunar adw-gtk-theme papirus-icon-theme nwg-look zsh zsh-theme-powerlevel10k zsh-autosuggestions zsh-autocomplete ttf-jetbrains-mono-nerd cava

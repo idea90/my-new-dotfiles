ae8afcaacacb59bc42cdc9a343d9e3adc6ac7850
idea90 <148615429+idea90@users.noreply.github.com>
2025-03-04 17:40:49 +0700

    add brightnessctl

--- install-deps.sh
+++ install-deps.sh
@@ -1,2 +1,2 @@
 yay -Syu
-yay -S hyprland sddm waybar matugen network-manager-applet swww alacritty xed vim rofi-wayland wlogout swaync nemo papirus-icon-theme nwg-look fish linutil-bin brightness-ctl ttf-roboto-mono-nerd mpv gvfs spotify fish starship
+yay -S hyprland sddm waybar matugen network-manager-applet swww alacritty xed vim rofi-wayland wlogout swaync nemo papirus-icon-theme nwg-look fish linutil-bin brightnessctl ttf-roboto-mono-nerd mpv gvfs spotify fish starship
